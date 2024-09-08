235625a0cf717c7945beb693d99ea412d034cc0f
charans29 <smodugula@csuchico.edu>
2024-09-08 13:34:56 -0400

    ft:deploying

---
+++ components/BlinkCard.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Action, Blink } from "@dialectlabs/blinks";
+import { useEffect, useState } from "react";
+import '@dialectlabs/blinks/index.css';
+
+function BlinkCard() {
+    const [websiteText, setWebsiteText] = useState<string>("");
+    const [action, setAction] = useState<Action | undefined>();
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const urlParams = new URLSearchParams(window.location.search);
+            const actionQuery = urlParams.get('action');
+            if (actionQuery) {
+                const fetchData = async () => {
+                    try {
+                        const actionUrl = new URL(actionQuery);
+                        setWebsiteText(actionUrl.hostname);
+                        const action = await Action.fetch(actionQuery);
+                        setAction(action);
+                    } catch (error) {
+                        console.error('Error fetching data:', error);
+                    }
+                };
+                fetchData();
+            }
+        }
+    }, []);
+
+    return (
+        <div style={{
+            maxWidth: '450px',
+            margin: '0 auto',
+            padding: "10px",
+            width: '100%'
+        }}>
+            {action && (
+                <>
+                    <Blink stylePreset='custom' action={action} websiteText={websiteText} />
+                </>
+            )}
+        </div>
+    );
+}
+
+export default BlinkCard;
