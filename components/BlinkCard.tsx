34ea06486409bff1f8047b2268f5870a54d2c729
charans29 <smodugula@csuchico.edu>
2024-09-08 18:16:11 -0400

    ft:rndrng-chk-2 blink@canvas

--- components/BlinkCard.tsx
+++ components/BlinkCard.tsx
@@ -1,23 +1,38 @@
 "use client";
 import { Action, Blink, useActionsRegistryInterval } from "@dialectlabs/blinks";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import '@dialectlabs/blinks/index.css';
+import { CanvasClient } from "@dscvr-one/canvas-client-sdk";
 
 function BlinkCard() {
     const [websiteText, setWebsiteText] = useState<string>("");
     const [action, setAction] = useState<Action | undefined>();
     const { isRegistryLoaded } = useActionsRegistryInterval();
+    const containerRef = useRef<HTMLDivElement>(null);
+    const canvasClientRef = useRef<CanvasClient | undefined>();
+    const [canvasClient, setCanvasClient] = useState<CanvasClient | null>(null);
 
     const isIframe = () => {
         try {
-          return window.self !== window.top;
+            return window.self !== window.top;
         } catch (e) {
-          return true;
+            return true;
         }
     };
 
     useEffect(() => {
         if (isIframe() || typeof window !== 'undefined') {
+            if (isIframe()) {
+                let client = new CanvasClient();
+                canvasClientRef.current = client;
+                setCanvasClient(client);
+            }
+            const resizeObserver = new ResizeObserver(() => {
+                canvasClientRef?.current?.resize();
+            });
+            if (containerRef.current) {
+                resizeObserver.observe(containerRef.current);
+            }
             const urlParams = new URLSearchParams(window.location.search);
             const actionQuery = urlParams.get('action');
             if (actionQuery) {
@@ -33,19 +48,30 @@ function BlinkCard() {
                 };
                 fetchData();
             }
+            return () => {
+                if (containerRef.current) {
+                    resizeObserver.unobserve(containerRef.current);
+                }
+            };
         }
     }, []);
 
     return (
-        <div style={{
-            maxWidth: '450px',
-            margin: '0 auto',
-            padding: "10px",
-            width: '100%'
-        }}>
+        <div ref={containerRef}
+            style={{
+                maxWidth: '450px',
+                margin: '0 auto',
+                padding: "10px",
+                width: '100%'
+            }}
+            className="text-center"
+        >
             {isRegistryLoaded && action && (
                 <>
                     <Blink stylePreset='custom' action={action} websiteText={websiteText} />
+                    <p className="text-white font-mono">
+                        {canvasClient ? 'Canvas client initialized' : 'Blink...!'}
+                    </p>
                 </>
             )}
         </div>
