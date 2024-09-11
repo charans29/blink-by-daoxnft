0cce206073226a208f767e508e88a33fb801ff0a
charans29 <smodugula@csuchico.edu>
2024-09-10 22:18:19 -0400

    ft:checking blink @addedadapter

--- components/BlinkCard.tsx
+++ components/BlinkCard.tsx
@@ -1,54 +1,155 @@
 "use client"
-import React, { useEffect, useRef, useState } from "react";
-import '@dialectlabs/blinks/index.css';
-import { Action, ActionContainer } from "@dialectlabs/blinks";
-import { CanvasClient } from "@dscvr-one/canvas-client-sdk";
+// import React, { useEffect, useRef, useState } from "react";
+// import '@dialectlabs/blinks/index.css';
+// import { Action, ActionContainer } from "@dialectlabs/blinks";
+// import { CanvasClient } from "@dscvr-one/canvas-client-sdk";
 
-function BlinkCard() {
-  const [websiteText, setWebsiteText] = useState<string>("");
-  const [action, setAction] = useState<Action | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const containerRef = useRef<HTMLDivElement | null>(null);
+// function BlinkCard() {
+//   const [websiteText, setWebsiteText] = useState<string>("");
+//   const [action, setAction] = useState<Action | undefined>(undefined);
+//   const [loading, setLoading] = useState<boolean>(true);
+//   const [error, setError] = useState<string | null>(null);
+//   const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const canvasClient = useRef<CanvasClient | null>(null);
+//   const canvasClient = useRef<CanvasClient | null>(null);
 
-  useEffect(() => {
-    if (window.self !== window.top) {
-      canvasClient.current = new CanvasClient();
-    }
-  }, []);
+//   useEffect(() => {
+//     if (window.self !== window.top) {
+//       canvasClient.current = new CanvasClient();
+//     }
+//   }, []);
 
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(() => {
+//   useEffect(() => {
+//     const resizeObserver = new ResizeObserver(() => {
      
+//     });
+
+//     if (containerRef.current) {
+//       resizeObserver.observe(containerRef.current);
+//     }
+
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const actionQuery = urlParams.get('action') ?? "https://da0-x-nft.vercel.app/api/join-dao-action?nft_id=28&mbrs=4&frcn=4";
+    
+//     if (actionQuery) {
+//       const fetchData = async () => {
+//         try {
+//           const actionUrl = new URL(actionQuery);
+//           setWebsiteText(actionUrl.hostname);
+//           const fetchedAction = await Action.fetch(actionQuery);
+//           console.log("A__X__Ns: ", fetchedAction)
+//           setAction(fetchedAction);
+//         } catch (fetchError) {
+//           console.error('Error fetching data:', fetchError);
+//           setError('Failed to load Blink content.');
+//         } finally {
+//           setLoading(false);
+//         }
+//       };
+//       fetchData();
+//     } else {
+//       setLoading(false);
+//     }
+
+//     return () => {
+//       if (containerRef.current) {
+//         resizeObserver.unobserve(containerRef.current);
+//       }
+//     };
+//   }, []);
+
+//   if (loading) {
+//     return <div className="text-green-500">Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div className="text-red-500">Error: {error}</div>;
+//   }
+
+//   return (
+//         <div
+//             ref={containerRef}
+//             style={{
+//                 maxWidth: '450px',
+//                 margin: '0 auto',
+//                 padding: "10px",
+//                 width: '100%',
+//             }}
+//             className="text-center w-10/12 flex flex-row space-y-4 justify-center items-center"
+//         >
+//             {action && (
+//                 <>
+//                 <ActionContainer stylePreset="custom" action={action} websiteText={websiteText} />
+//                 <p className="text-white font-mono">
+//                     {canvasClient.current ? 'Canvas client initialized by DAOxNFT' : ''}
+//                 </p>
+//                 </>
+//             )}
+//         </div>
+//   );
+// }
+
+// export default BlinkCard;
+
+
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Action,
+  ActionContainer,
+} from "@dialectlabs/blinks";
+import '@dialectlabs/blinks/index.css';
+
+import { CanvasClient } from "@dscvr-one/canvas-client-sdk";
+import { CanvasAdapter, isIframe } from "@/adapter/canvas-adapter";
+
+const BlinkCard = () => {
+  const [action, setAction] = useState<Action | null>(null);
+  const [iframe, setIsInIframe] = useState(false);
+  const [websiteUrl, setWebsiteUrl] = useState("");
+  const [websiteText, setWebsiteText] = useState("");
+  const containerRef = useRef<HTMLDivElement>(null);
+  const canvasClientRef = useRef<CanvasClient | undefined>();
+  
+  useEffect(() => {
+    if(iframe) {
+      canvasClientRef.current = new CanvasClient();
+    };
+
+    setIsInIframe(isIframe());
+    const adapter = iframe ? new CanvasAdapter() : undefined;
+
+    const fetchAction = async () => {
+      const url = new URL(window.location.href);
+      
+      const actionParam = url.searchParams.get('action') ?? "https://da0-x-nft.vercel.app/api/join-dao-action?nft_id=20&mbrs=4&frcn=4";
+      
+      if (actionParam) {
+        try {
+          const actionUrl = new URL(actionParam);
+          
+          setWebsiteUrl(actionUrl.toString());
+          setWebsiteText(actionUrl.host);
+
+          const action = await Action.fetch(
+            actionParam,
+            adapter
+          );
+          setAction(action);
+        } catch (error) {
+          console.error("Invalid action URL:", error);
+        }
+      } else {
+        console.error("No action parameter provided in URL");
+      }
+    };
+    fetchAction();
+
+    const resizeObserver = new ResizeObserver(() => {
+      canvasClientRef?.current?.resize();
     });
 
     if (containerRef.current) {
       resizeObserver.observe(containerRef.current);
-    }
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const actionQuery = urlParams.get('action') ?? "https://da0-x-nft.vercel.app/api/join-dao-action?nft_id=28&mbrs=4&frcn=4";
-    
-    if (actionQuery) {
-      const fetchData = async () => {
-        try {
-          const actionUrl = new URL(actionQuery);
-          setWebsiteText(actionUrl.hostname);
-          const fetchedAction = await Action.fetch(actionQuery);
-          console.log("A__X__Ns: ", fetchedAction)
-          setAction(fetchedAction);
-        } catch (fetchError) {
-          console.error('Error fetching data:', fetchError);
-          setError('Failed to load Blink content.');
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchData();
-    } else {
-      setLoading(false);
     }
 
     return () => {
@@ -58,35 +159,34 @@ function BlinkCard() {
     };
   }, []);
 
-  if (loading) {
-    return <div className="text-green-500">Loading...</div>;
-  }
+  const exampleCallbacks = {
+    onActionMount: (action: Action, originalUrl: string, type: "trusted" | "malicious" | "unknown") => {
+      console.log("Action mounted:", action, originalUrl, type);
+    },
+  };
 
-  if (error) {
-    return <div className="text-red-500">Error: {error}</div>;
-  }
+  const exampleSecurityLevel = "only-trusted";
+
+  const containerStyle = {
+    maxWidth: '450px',
+    margin: '0 auto',
+    width: '100%'
+  };
 
   return (
-        <div
-            ref={containerRef}
-            style={{
-                maxWidth: '450px',
-                margin: '0 auto',
-                padding: "10px",
-                width: '100%',
-            }}
-            className="text-center w-10/12 flex flex-row space-y-4 justify-center items-center"
-        >
-            {action && (
-                <>
-                <ActionContainer stylePreset="custom" action={action} websiteText={websiteText} />
-                <p className="text-white font-mono">
-                    {canvasClient.current ? 'Canvas client initialized by DAOxNFT' : ''}
-                </p>
-                </>
-            )}
-        </div>
+    <div ref={containerRef} style={containerStyle}>
+      {action && (
+        <ActionContainer
+          action={action}
+          websiteUrl={websiteUrl}
+          websiteText={websiteText}
+          callbacks={exampleCallbacks}
+          securityLevel={exampleSecurityLevel}
+          stylePreset="custom"
+        />
+      )}
+    </div>
   );
-}
+};
 
 export default BlinkCard;
